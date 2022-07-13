c7b0350924239a5a8dc83f890365a0904d96f6b6
Liyou <1085066875@qq.com>
2022-07-13 14:33:32 +0800

    通过格式化的输入自动创建目录并批量对图片去畸变

--- distor_img_batches.m
+++ distor_img_batches.m
@@ -1,8 +1,13 @@
-
-souredir = "";
+dirtory = "dir";
+formatS =  "//xxx/%s/c/";
+formatT =  "//xxx/%s/undistored/c/";
+souredir = sprintf(formatS, dirtory);
 suffix = "jpg";
 files = dir([souredir+'*.'+ suffix]);
-targetdir = "";
+targetdir = sprintf(formatT, dirtory);
+if exist(targetdir) == 0
+    mkdir(targetdir)
+end
 parfor i=1:length(files)
     I = imread(souredir+files(i).name);
     J1 = undistortImage(I,cameraParams);
